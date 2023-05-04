09ae5132057a9d39ba97f94839b46de032ee1e9d
Nguyen Thuc <nguyenthuc88@gmail.com>
2023-05-04 17:21:46 +0700

    test

--- src/test.js
+++ src/test.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
-    
+
 describe('App component', () => {
   //Test 1: Ví dụ test quản lý state cơ bản
   it('starts with a count of 0', () => {
@@ -11,6 +11,7 @@ describe('App component', () => {
     expect(text).toEqual('Count: 0');
   });
   //Test 2: Ví dụ test tương tác của người dùng
+
   it('increments count by 1 when the increment button is clicked', () => {
     const wrapper = shallow(<App />);
     const incrementBtn = wrapper.find('button.increment');
